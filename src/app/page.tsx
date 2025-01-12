e3467879c5b9eda2ac99ba53d0e10e94d635abae
Femi Adigun <babaphemy@gmail.com>
2025-01-11 23:58:19 -0600

    deleting ununsed repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { link } from "fs";
 import {
   BadgeCheck,
   BookOpen,
